ba16ea56b9987863403089f1c1f7bb9384234b1c
gustavofbc <gustavo.felipebc@gmail.com>
2021-03-28 21:30:16 -0300

    implementaçao inicial da tela de apresentaçoes

--- voyager/src/App.js
+++ voyager/src/App.js
@@ -7,6 +7,7 @@ import Routes from './routes';
 function App() {
   return (
     <div className="App">
+      <p>menu aqui</p>
       <Routes />
     </div>
   );
--- voyager/src/pages/Apresentation/index.js
+++ voyager/src/pages/Apresentation/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import logo from '../../assets/Logo.svg'
+
+import "./styles.css";
 
 export default function Apresentation() {
     return (
-        <h1>Hello voyager</h1>
+        <div className="cointainer">
+            <img src={logo} alt="Voyager logo"/>
+            
+            <h2>
+                Encontre o voo ideal para sua viagem!
+            </h2>
+
+            <div className="sections">
+                <section>
+                    <select>
+                        <option id="0">Selecione a origem</option>
+                    </select>
+
+                    <label for="ida">Data: 
+                        <input type="date" id="ida"/>
+                    </label>
+                </section>
+
+                <section>
+                    <select>
+                        <option id="0">Selecione o destino</option>
+                    </select>
+
+                    <label for="volta">Data: 
+                        <input type="date" id="volta"/>
+                    </label>
+                </section>
+            </div>
+
+        </div>
     );
 }
